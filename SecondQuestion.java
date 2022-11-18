9643f54488918e4a72dcae960711c286888b55b2
duggemvs <111142028+duggemvs@users.noreply.github.com>
2022-11-18 14:32:02 +0530

    Add files via upload

---
+++ SecondQuestion.java
@@ -0,0 +1,20 @@
+package Second;
+
+
+import java.util.Scanner;
+public class SecondQuestion
+{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n=s.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				System.out.print(i+" ");
+			
+			}
+			System.out.println();
+		}
+	}
+}
